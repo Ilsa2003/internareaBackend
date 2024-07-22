195040085440d6c43f9f517db9a8aa1e7ea2f55f
Ilsa <ilsamemon14@gmail.com>
2024-07-22 15:13:20 +0530

    init

--- Routes/index.js
+++ Routes/index.js
@@ -22,20 +22,21 @@
 
 const express = require("express");
 const router = express.Router();
-const ApplicationRoute = require("./applicationRoute");
-const intern = require("./internshipRout");
-const job = require("./jobRoute");
-const admin = require("./admin");
+const applicationRoute = require("./applicationRoute");
+const internshipRoute = require("./internshipRout");
+const jobRoute = require("./jobRoute");
+const adminRoute = require("./admin");
 const avatarRoute = require("./avatarRoute");
 
 router.get("/", (req, res) => {
     res.send("This is the backend");
 });
 
-router.use('/application', ApplicationRoute);
-router.use('/internship', intern);
-router.use('/job', job);
-router.use('/admin', admin);
+router.use('/application', applicationRoute);
+router.use('/internship', internshipRoute);
+router.use('/job', jobRoute);
+router.use('/admin', adminRoute);
 router.use('/avatar', avatarRoute); // Avatar routes
 
 module.exports = router;
+
