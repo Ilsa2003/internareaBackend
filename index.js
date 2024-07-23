77089cf66e69ea743ab46eb27f57b1e27749944f
Ilsa <ilsamemon14@gmail.com>
2024-07-23 12:05:36 +0530

    init

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const { connect } = require('./db');
-// const avatarRoute = require('./routes/avatarRoute');
-const avatarRoute = require(path.resolve(__dirname, 'routes', 'avatarRoute'));
+const avatarRoute = require('./routes/avatarRoute');
 const router = require('./routes/index');
 const otpRoutes = require('./routes/otpRoutes');
 // Load environment variables
