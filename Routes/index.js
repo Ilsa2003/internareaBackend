1d2b79af71e1307daa7a80ca9c19c3948fdeea60
Ilsa <ilsamemon14@gmail.com>
2024-07-24 15:56:15 +0530

    init

--- Routes/index.js
+++ Routes/index.js
@@ -1,41 +1,18 @@
-// const express = require("express")
-// const router = express.Router();
-// const ApplicationRoute = require("./ApplicationRoute")
-// const intern = require("./internshipRout")
-// const job = require("./jobRoute")
-// const admin = require("./admin")
-// const avatarRoute = require('./routes/avatarRoute');
-
-
-
-// router.get("/", (req, res) => {
-//     res.send("the is backend")
-// })
-// router.use('/application', ApplicationRoute);
-// router.use('/internship', intern);
-// router.use('/job', job);
-// router.use('/admin', admin);
-// app.use('/api/avatar', avatarRoute); // Avatar routes
-
-
-// module.exports = router;
-
-const express = require('express');
+const express = require("express")
 const router = express.Router();
-const ApplicationRoute = require('./ApplicationRoute');
-const intern = require('./internshipRout');
-const job = require('./jobRoute');
-const admin = require('./admin');
+const ApplicationRoute = require("./ApplicationRoute")
+const intern = require("./internshipRout")
+const job = require("./jobRoute")
+const admin = require("./admin")
 const avatarRoute = require('./avatarRoute');
 
-router.get('/', (req, res) => {
-    res.send('This is the backend');
-});
-
+router.get("/", (req, res) => {
+    res.send("the is backend")
+})
 router.use('/application', ApplicationRoute);
 router.use('/internship', intern);
 router.use('/job', job);
 router.use('/admin', admin);
-router.use('/api/avatar', avatarRoute); // Avatar routes
+app.use('/api/avatar', avatarRoute); // Avatar routes
 
 module.exports = router;
